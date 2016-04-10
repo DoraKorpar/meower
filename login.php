28c97997c6ec750150f8d84d47e35c603456728c
dora <dora.korpar@gmail.com>
2016-04-10 16:50:52 -0700

    still working on login

--- login.php
+++ login.php
@@ -1,1 +1,21 @@
-<!DOCTYPE html>
+<?php
+	session_start();
+
+	if ($_GET['login']) {
+
+		if ($_POST['username'] == 'USERNAME' && $_POST['password'] == 'PASSWORD') {
+
+			$_SESSION['loggedin'] = 1;
+
+			header("Location: index.php");
+			exit;
+		} else echo "Information doesn't match";
+	}
+?>
+
+Log in:
+<form action="?login=1" method="post">
+Username: <input type="text" name="login" />
+Password: <input type="password" name="password" />
+<input type="submit" />
+</form>
