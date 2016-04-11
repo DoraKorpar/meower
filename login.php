50858997838e9f0604fa18909ad01abbffb70a43
dora <dora.korpar@gmail.com>
2016-04-10 19:10:17 -0700

    added login css

--- login.php
+++ login.php
@@ -26,19 +26,29 @@
 
 <html>
 <head>
-	<title></title>
+	<title>Log-In Page</title>
+	<meta charset="UTF-8">
+	<link type="text/css" rel="stylesheet" href="login.css" />
+	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Architects+Daughter' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<div>
-	<h2>Log in:</h2>
-	<form action="index.php" method="post" enctype="multipart/form-data">
-		Username: <br/>
-		<input type="text" name="login" /><br/>
-		Password: <br/>
-		<input type="password" name="password" /><br/>
-		<input type="submit" value="Login" name="Submit" />
-	</form>
-</div>
+	<div id="header">
+		<div id="logo">
+			<img id="kitten" src="https://cloud.githubusercontent.com/assets/15206187/14062361/7b399324-f359-11e5-9598-d712f8cb36bf.png" alt="meower logo black color" />
+			<h1 id="tagline">MEOWER</h1>
+		</div>
+	</div>
+	<div id="form-div">
+		<h2>Please log in below</h2>
+		<form action="index.php" method="post" enctype="multipart/form-data">
+			Username: <br/>
+			<input type="text" name="login" /><br/>
+			Password: <br/>
+			<input type="password" name="password" /><br/>
+			<input type="submit" value="Login" name="Submit" />
+		</form>
+	</div>
 </body>
 </html>
 
