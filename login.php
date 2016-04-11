fb379479a4551eb505e9b8d46af3c0f14687638d
dora <dora.korpar@gmail.com>
2016-04-10 17:41:27 -0700

    progress on login

--- login.php
+++ login.php
@@ -1,21 +1,48 @@
 <?php
+
 	session_start();
 
-	if ($_GET['login']) {
+	if ($_POST['login']) {
 
-		if ($_POST['username'] == 'USERNAME' && $_POST['password'] == 'PASSWORD') {
+		$username = "User";
+		$password = "password";
+		$id = "1";
+
+		$use_input = strip_tags($_POST['login']);
+		$pass_input = strip_tags($_POST['password']);
+
+		if ($_POST['login'] == $username && $_POST['password'] == $password) {
 
 			$_SESSION['loggedin'] = 1;
+			$_SESSION['login'] = $username;
+			$_SESSION['id'] = $id;
 
 			header("Location: index.php");
-			exit;
-		} else echo "Information doesn't match";
+		} else {
+			echo "Invalid information";
+		}
 	}
 ?>
 
-Log in:
-<form action="?login=1" method="post">
-Username: <input type="text" name="login" />
-Password: <input type="password" name="password" />
-<input type="submit" />
-</form>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div>
+	<h2>Log in:</h2>
+	<form action="index.php" method="post" enctype="multipart/form-data">
+		Username: <br/>
+		<input type="text" name="login" /><br/>
+		Password: <br/>
+		<input type="password" name="password" /><br/>
+		<input type="submit" value="Login" name="Submit" />
+	</form>
+</div>
+</body>
+</html>
+
+<?php
+
+
+?>
